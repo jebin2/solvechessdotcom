06a995b46fa39ce91aacf2554b95c15e92981a49
jebin2 <jebineinstein@gmail.com>
2026-04-02 15:45:04 +0000

    pip

---
+++ run_pipelines.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+REELFORGE_DIR="$SCRIPT_DIR"
+
+while true; do
+    echo "[$(date)] Running reelforge..."
+    cd "$REELFORGE_DIR" && bash run_app.sh --onepass
+
+    echo "[$(date)] Sleeping for 60 seconds..."
+    sleep 60
+done
